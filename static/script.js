4e3f7d86219639beabc15f3c5c1aecfa1a015af3
Akshay <akshayc1991@gmail.com>
2021-06-11 00:28:45 +0530

    Final commit

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.getElementById("button1").addEventListener("click", function(){
         let value = document.getElementById("uri").value
         const div = document.getElementById("pred")
-        fetch('https://tweetsentiment-app.herokuapp.com/prediction',{
+        fetch('http://127.0.0.1:5000/prediction',{
               method:'post',
               headers:{'Content-Type':'application/json'},
               body:JSON.stringify({
