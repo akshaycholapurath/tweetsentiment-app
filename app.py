54e3534bac06aa846fc2c7adc33d1cd47b84ba9d
Akshay <akshayc1991@gmail.com>
2021-06-11 13:38:48 +0530

    Bugs fix

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask import Flask,request, render_template
 from flask import jsonify
 import tensorflow as tf
 import numpy as np
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 model = tf.keras.models.load_model('sentiment_model1')
 
 
 app = Flask(__name__)
-CORS(app)
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 class_name = ['negative','postive']
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/prediction',methods=['POST'])
+@cross_origin()
 def pred():
     data = request.json
     text = data['text']
